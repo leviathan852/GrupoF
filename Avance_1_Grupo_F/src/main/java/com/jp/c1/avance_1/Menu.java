d1e9228f7c236e853a1284fdf53e716f918f8125
leviathan852 <fretana852@gmail.com>
2025-07-23 18:29:04 -0600

    Avance de ClienteHotel
    Desarrollo del array para los clientes, como cantidad maxima de 100, id unico, automatico y el ingreso de datos como su almacenamiento en la lista

--- Avance_1_Grupo_F/src/main/java/com/jp/c1/avance_1/Menu.java
+++ Avance_1_Grupo_F/src/main/java/com/jp/c1/avance_1/Menu.java
@@ -7,6 +7,12 @@ import java.util.Random;
 import javax.swing.JOptionPane;
         
 public class Menu {
+
+    private static final int MAX_CLIENTES = 100;
+    private static ClienteHotel[] listaClientes = new ClienteHotel[MAX_CLIENTES];
+    private static int cantidadClientes = 0;
+    private static int contadorIdClientes = 1;
+
     public static void mostrarMenu() {
         int opcion;
 
@@ -27,13 +33,17 @@ public class Menu {
 
                 switch (opcion) {
                     case 1:
-                        //registrarCliente();
+                        registrarCliente();
+                        break;
                     case 2: 
                         //hacerReserva();
+                        break;
                     case 3:
                         //cancelarSalida();
+                        break;
                     case 4:
                         //mostrarReportes();
+                        break;
                     case 5: 
                         JOptionPane.showMessageDialog(null, "Gracias por usar el sistema");
                         return;
@@ -43,4 +53,46 @@ public class Menu {
             }
         } while (true);
     }
+
+
+    //Registro de Clientes
+
+    private static String generarIdCliente() {
+        String id = String.format("CL-%04d", contadorIdClientes); // id formato -> CL-0XXX
+        contadorIdClientes++;
+        return id;
+    }
+
+    private static void registrarCliente() {
+        if (cantidadClientes >= MAX_CLIENTES) {
+            JOptionPane.showMessageDialog(null, "Limite de clientes alcanzado.");
+            return;
+        }
+
+        String id = generarIdCliente();
+
+        //Validacion de Id
+        for (int i = 0; i < cantidadClientes; i++) {
+            if (listaClientes[i].getIdCliente().equals(id)) {
+                JOptionPane.showMessageDialog(null, "Error: ID duplicado.");
+                return;
+            }
+        }
+
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre:");
+        if (nombre == null || nombre.isBlank()) return;
+
+        String correo = JOptionPane.showInputDialog("Ingrese el correo:");
+        if (correo == null || correo.isBlank()) return;
+
+        String telefono = JOptionPane.showInputDialog("Ingrese el telefono:");
+        if (telefono == null || telefono.isBlank()) return;
+
+        ClienteHotel nuevo = new ClienteHotel(nombre, correo, id, telefono);
+        listaClientes[cantidadClientes] = nuevo;
+        cantidadClientes++;
+
+        JOptionPane.showMessageDialog(null, "Cliente registrado con exito.\nID asignado: " + id);
+    }
+
 }
